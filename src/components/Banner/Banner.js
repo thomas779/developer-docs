f4bfcf568e0346c52fef035d018bc80f7e9adffa
marc <sed@protonmail.com>
2023-08-23 17:55:18 +0200

    add new banner in docs

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,38 @@
+import React from "react";
+// import logo from '../../../static/img/logo-dark.png';
+import styles from "./banner.module.css";
+
+// component banner and css written by walletconnect docs
+const Banner = () => {
+  return (
+    <div className={styles.banner__container}>
+      <div className={styles.banner__text}>
+        <h2>Candide.Dev</h2>
+        <p>
+          Candide provides toolkits to build and leverage smart
+          wallets. Build on robust Account Abstraction Infrastructure on all EVM.
+        </p>
+        {/* <a href="./wallet/guides/getting-started">
+          Get started
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+            />
+          </svg>
+        </a> */}
+      </div>
+      <div className={styles.banner__backdrop} />
+      {/* <img className={styles.banner__image} src={logo} alt="Candide Logo" /> */}
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import homePageFeatures from '../homePageFeatures'
 import styles from './index.module.css';
+import Banner from '../components/Banner/Banner';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext()
@@ -12,6 +13,7 @@ export default function Home() {
       title={`Account Abstraction Developer Documentation`}
       description="Building open-source software for ERC-4337 Account Abstraction"
     >
+      <Banner />
       <main>
         <div className={styles.container}>
           <div className={styles.row}>
