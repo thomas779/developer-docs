de58f51022f4c0e0b92e12790f0669a995b83a68
marc <sed@protonmail.com>
2023-10-20 16:45:43 +0200

    set trailingSlash to true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -145,6 +145,7 @@ const config = {
         indexName: 'candide',
         insights: true,
         contextualSearch: true,
+        trailingSlash: true,
         // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'external\\.com|domain\\.com',
   
