cc8dd4f0bba8d41b35926b1b2b85e93373bb3096
marc <sed@protonmail.com>
2024-04-29 12:44:08 +0200

    add new paymaster tokens supported

---
+++ src/data/tokens-list.ts
@@ -0,0 +1,155 @@
+// mainnet tokens
+export const arbitrumList = [
+  {
+    token: "DAI",
+    logo: "/img/tokens/dai-logo.png",
+    address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+  },
+  {
+    token: "USDC (Native)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+  },
+  {
+    token: "USDT",
+    logo: "/img/tokens/usdt-logo.svg",
+    address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+  },
+];
+
+export const baseList = [
+  {
+    token: "DAI",
+    logo: "/img/tokens/dai-logo.png",
+    address: "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
+  },
+  {
+    token: "USDC (Native)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  },
+];
+
+export const bscList = [
+  {
+    token: "BSC-USD (ex USDT)",
+    logo: "/img/tokens/busdt-logo.png",
+    address: "0x55d398326f99059fF775485246999027B3197955",
+  },
+  {
+    token: "USDC (Binance-Peg)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+  },
+];
+
+export const polygonList = [
+  {
+    token: "DAI",
+    logo: "/img/tokens/dai-logo.png",
+    address: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
+  },
+  {
+    token: "USDC (Native)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+  },
+  {
+    token: "USDC.e (Bridged)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+  },
+  {
+    token: "USDT",
+    logo: "/img/tokens/usdt-logo.svg",
+    address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+  },
+];
+export const celoList = [
+  {
+    token: "cUSD",
+    logo: "/img/tokens/cUSD.png",
+    address: "0x765de816845861e75a25fca122bb6898b8b1282a",
+  },
+  {
+    token: "cEUR",
+    logo: "/img/tokens/cEUR.png",
+    address: "0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73",
+  },
+  {
+    token: "USDC (Native)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0xcebA9300f2b948710d2653dD7B07f33A8B32118C",
+  },
+  {
+    token: "USDT",
+    logo: "/img/tokens/usdt-logo.svg",
+    address: "0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e",
+  },
+];
+export const optimismList = [
+  {
+    token: "DAI",
+    logo: "/img/tokens/dai-logo.png",
+    address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+  },
+  {
+    token: "USDC (Bridged)",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
+  },
+  {
+    token: "USDC Native",
+    logo: "/img/tokens/usdc-logo.svg",
+    address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+  },
+  {
+    token: "USDT",
+    logo: "/img/tokens/usdt-logo.svg",
+    address: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58",
+  },
+];
+
+// testnet tokens
+export const alfajoresList = [
+  {
+    token: "Candide Test Token",
+    logo: "/img/tokens/ctt-logo.png",
+    address: "0xFa5854FBf9964330d761961F46565AB7326e5a3b",
+  },
+];
+export const amoyList = [
+  {
+    token: "Candide Test Token",
+    logo: "/img/tokens/ctt-logo.png",
+    address: "0xE0BD422189D77cD1AC77C520B363a7FA649FFdf1",
+  },
+];
+export const arbitrumSepoliaList = [
+  {
+    token: "Candide Test Token",
+    logo: "/img/tokens/ctt-logo.png",
+    address: "0xE0BD422189D77cD1AC77C520B363a7FA649FFdf1",
+  },
+];
+export const baseSepoliaList = [
+    {
+      token: "Candide Test Token",
+      logo: "/img/tokens/ctt-logo.png",
+      address: "0xE0BD422189D77cD1AC77C520B363a7FA649FFdf1",
+    },
+  ];
+export const sepoliaList = [
+  {
+    token: "Candide Test Token",
+    logo: "/img/tokens/ctt-logo.png",
+    address: "0xFa5854FBf9964330d761961F46565AB7326e5a3b",
+  },
+];
+export const optimismSepoliaList = [
+  {
+    token: "Candide Test Token",
+    logo: "/img/tokens/ctt-logo.png",
+    address: "0xE0BD422189D77cD1AC77C520B363a7FA649FFdf1",
+  },
+];
