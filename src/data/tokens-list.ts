a182c82f5e6b60bffc28d13c5e17d1a641fbc3f0
marc <sed@protonmail.com>
2024-08-31 21:20:17 +0300

    Remove usdt support from ethereum mainnet token paymaster

--- src/data/tokens-list.ts
+++ src/data/tokens-list.ts
@@ -104,11 +104,6 @@ export const ethereumList = [
     logo: "/img/tokens/usdc-logo.svg",
     address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
   },
-  {
-    token: "USDT",
-    logo: "/img/tokens/usdt-logo.svg",
-    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-  },
 ];
 
 export const gnosisList = [
