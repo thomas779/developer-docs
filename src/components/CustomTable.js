8fa428b86dfa777070eb312ec63d2957f13f829b
marc <sed@protonmail.com>
2024-11-11 06:36:39 +0100

    New Sidebar improvements and others

--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -31,6 +31,33 @@ export function BundlerEndPointTable({ items }) {
   );
 }
 
+export function ChainListTable({ items }) {
+  return (
+    <Table
+      items={items}
+      leftHeading="Network"
+      rightHeading="Chain ID"
+      renderLeftItem={(item) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start center",
+          }}
+        >
+          <img
+            src={item.logo}
+            style={{
+              width: "25px",
+              marginRight: "10px",
+            }}
+          />
+          <span>{item.network}</span>
+        </div>
+      )}
+      renderRightItem={(item) =>  <span>{item.chainId}</span>}
+    />
+  );
+}
 export function PaymasterSupportedToken({ items }) {
   return (
     <Table
